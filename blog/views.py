c4a9174c4cd80853cc00b8da1bf5e1cf3be088d4
varan5 <varan2.bhalla@gmail.com>
2021-07-11 13:59:30 +0530

    Inherited new signup form from the default

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render, HttpResponseRedirect
-
+from .forms import SignUpForm
 
 def home(request):
     return render(request, "blog/home.html")
 
+
 def about(request):
     return render(request, "blog/about.html")
+
 
 def contact(request):
     return render(request, "blog/contact.html")
 
+
 def dashboard(request):
     return render(request, "blog/dashboard.html")
 
+
 def user_signup(request):
-    return render(request, "blog/signup.html")
+    form = SignUpForm()
+    return render(request, "blog/signup.html", {"form": form})
+
 
 def user_login(request):
     return render(request, "blog/login.html")
 
+
 def user_logout(request):
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect("/")
